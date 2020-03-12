338726f975c85892ef52770cb25e4b13f2bcd877
Nimzyow <n_soufiani@hotmail.com>
2020-03-12 14:54:13 +0000

    added delete a bookmark, added update bookmark,added edit.erb

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 require_relative './lib/bookmark.rb'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions, :method_override
+
   get '/' do
     "Stephan the greats, Bookmark Manager"
   end
@@ -20,6 +23,23 @@ class BookmarkManager < Sinatra::Base
   get '/bookmarks' do
     @bookmarks = Bookmark.all
     erb :'bookmarks/index'
+  end
+
+  delete "/bookmarks/:id" do
+    #easy to delete specific bookmark of specific user.
+    Bookmark.delete(id: params[:id])
+    redirect("/bookmarks")
+  end
+
+  get "/bookmarks/:id/edit" do
+    @bookmark_id = params[:id]
+    erb :"bookmarks/edit"
+  end
+
+  patch "/bookmarks/:id" do
+    connection = PG.connect(dbname: "bookmark_manager_test")
+    connection.exec("UPDATE bookmarks SET url = '#{params[:url]}', title = '#{params[:title]}' WHERE id = '#{params[:id]}'")
+    redirect("/bookmarks")
   end
   
   run! if app_file == $PROGRAM_NAME
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -19,18 +19,45 @@ class Bookmark
     end
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map do |bookmark|
+      #iterate over the results from SELECT * FROM bookmarks and create an instance of Bookmark to generate all the different fields.
       Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
   end
 end
 
   def self.create(url:, title:)
     #return false unless is_url?(url)
+    #we choose which envioronemnt to connect to. test or normal?
       if ENV['ENVIRONMENT'] == 'test'
         connection = PG.connect(dbname: 'bookmark_manager_test')
       else
         connection = PG.connect(dbname: 'bookmark_manager')
       end
-    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+      #we initiate a kind of psql here and pass in standard SQL commands
+      result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
+
+    #we create a new instance of Bookmark with id, title and url
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.delete(id:)
+    #we choose which envioronemnt to connect to. test or normal?
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+    connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+  end
+
+  def self.update(id:, url:, title:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    result = connection.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
